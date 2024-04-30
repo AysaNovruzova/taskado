0a6292a02c2ecc0caa2363cefeacea3697d7a623
Aysa <bd7hwd97w@code.edu.az>
2024-04-30 11:17:12 +0400

    task

---
+++ taskadonet/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace taskadonet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string con = "server= DESKTOP-80LIQDV\\SQLEXPRESS01"; database = adonet; Trusted_connection = true; integrated security = true;
+            //    using (SqlConnection connektion = new SqlConnection(con))
+            //    {
+            //        connektion.Open();
+
+            //        string command = "insert into users values ('Student') ";
+            //        SqlCommand cmd = new SqlCommand(command, connektion);
+            //        int result = cmd.ExecuteNonQuery();
+            //        if (result > 0)
+            //        {
+            //            Console.WriteLine("data elave olundu");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("mueyyen bir problem bas verdi");
+            //        }
+
+            //        string query = "selekt * from users";
+
+            //        SqlDataAdapter adapter = new SqlDataAdapter (query, connektion);
+            //        DataTable dataTable = new DataTable();
+            //        adapter.Fill(dataTable);
+
+            //        foreach (DataRow item in dataTable.Rows) 
+            //        {
+            //            Console.WriteLine(item[0]+" "+ item[1]);
+            //        }
+
+            //    }
+            //}
+        }
+    }
+
+}
